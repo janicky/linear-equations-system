2384de715591499bfc4f3ac3d82eaa9f1f2b7531
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-11 17:28:49 +0200

    Add Configuration file

---
+++ src/main/java/Configuration.java
@@ -0,0 +1,12 @@
+public class Configuration {
+
+    public double[][] matrix = {
+            { 3.45, 0.78, -0.97 },
+            { 0.78, 2.63, -0.89 },
+            { -0.97, -0.89, 2.41 }
+    };
+
+    public double[] b = {
+            3.229, 4.026, 5.030
+    };
+}
