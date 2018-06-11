4dfc954b69504382b70e151b937d4d661a6305aa
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-11 18:03:29 +0200

    Add Gauss method class

---
+++ src/main/java/Gauss.java
@@ -0,0 +1,22 @@
+public class Gauss {
+
+    private Configuration cfg;
+    private double[] results;
+
+    public Gauss() {
+        cfg = new Configuration();
+    }
+
+    private double[][] triangularMatrix() {
+        double[][] t_matrix = cfg.getMatrix();
+
+        for (int i = 0; i < cfg.getSize(); i++) {
+            for (int j = 0; j < cfg.getSize(); j++) {
+                for (int k = i + 1; k <= cfg.getSize(); k++) {
+                    t_matrix[j][k] = (-t_matrix[j][i] / t_matrix[i][i]) * t_matrix[i][k];
+                }
+            }
+        }
+        return t_matrix;
+    }
+}
