bec89aa46e3def6b36936ccf6db6185ca11a6f45
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-11 18:00:50 +0200

    Move b to matrix array

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,13 +1,9 @@
 public class Configuration {
 
     private double[][] matrix = {
-            { 3.45, 0.78, -0.97 },
-            { 0.78, 2.63, -0.89 },
-            { -0.97, -0.89, 2.41 }
-    };
-
-    private double[] b = {
-            3.229, 4.026, 5.030
+            { 3.45, 0.78, -0.97, 3.229 },
+            { 0.78, 2.63, -0.89, 4.026 },
+            { -0.97, -0.89, 2.41, 5.030 }
     };
 
     public double[][] getMatrix() {
@@ -22,21 +18,13 @@ public class Configuration {
         }
     }
 
-    public double[] getB() {
-        return b;
-    }
-
-    public void setB(double[] b) {
-        this.b = b;
-    }
-
     public int getSize() {
         return matrix.length;
     }
 
     private boolean checkMatrix(double[][] matrix) {
         for (double[] row : matrix) {
-            if (row.length != getSize()) {
+            if (row.length != getSize() + 1) {
                 return false;
             }
         }
