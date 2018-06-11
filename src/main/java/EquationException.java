ca184aaff2d4c076035d36da1ece4d768e65ce06
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-11 22:58:42 +0200

    Add denominator zero exception

--- src/main/java/EquationException.java
+++ src/main/java/EquationException.java
@@ -1,6 +1,7 @@
 public class EquationException extends Exception {
 
     public static final String INVALID_MATRIX = "Niepoprawny rozmiar macierzy";
+    public static final String DENOMINATOR_ZERO = "Zero w mianowniku";
 
     public EquationException(String message) {
         super(message);
--- src/main/java/Gauss.java
+++ src/main/java/Gauss.java
@@ -9,10 +9,13 @@ public class Gauss {
         results = new double[cfg.getSize()];
     }
 
-    private double[][] triangularMatrix() {
+    private double[][] triangularMatrix() throws EquationException {
         t_matrix = cfg.getMatrix();
 
         for (int i = 0; i < cfg.getSize(); i++) {
+            if (Math.abs(t_matrix[i][i]) < cfg.getPrecision()) {
+                throw new EquationException(EquationException.DENOMINATOR_ZERO);
+            }
             for (int j = 0; j < cfg.getSize(); j++) {
                 for (int k = i + 1; k <= cfg.getSize(); k++) {
                     t_matrix[j][k] = (-t_matrix[j][i] / t_matrix[i][i]) * t_matrix[i][k];
@@ -22,21 +25,28 @@ public class Gauss {
         return t_matrix;
     }
 
-    private double[] calculateResults() {
+    private double[] calculateResults() throws EquationException {
         double nominator;
         for (int i = cfg.getSize() - 1; i >= 0; i--) {
-            nominator = t_matrix[i][cfg.getSize() - 1];
+            if (Math.abs(t_matrix[i][i]) < cfg.getPrecision()) {
+                throw new EquationException(EquationException.DENOMINATOR_ZERO);
+            }
+            nominator = t_matrix[i][cfg.getSize()];
             for (int j = cfg.getSize() - 1; j >= i + 1; j--) {
                 nominator -= t_matrix[i][j] * results[j];
             }
-            results[i] = (t_matrix[i][i] != 0 ? nominator / t_matrix[i][i] : Double.MAX_VALUE);
+            results[i] = nominator / t_matrix[i][i];
         }
         return results;
     }
 
     public void calculate() {
-        triangularMatrix();
-        calculateResults();
+        try {
+            triangularMatrix();
+            calculateResults();
+        } catch (EquationException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     public double[] getResults() {
