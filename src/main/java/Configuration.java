27fbc7e791b8bef8b5809ef2820d6717c05fb07d
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-11 17:32:16 +0200

    Add getters and setters to Configuration

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,12 +1,28 @@
 public class Configuration {
 
-    public double[][] matrix = {
+    private double[][] matrix = {
             { 3.45, 0.78, -0.97 },
             { 0.78, 2.63, -0.89 },
             { -0.97, -0.89, 2.41 }
     };
 
-    public double[] b = {
+    private double[] b = {
             3.229, 4.026, 5.030
     };
+
+    public double[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(double[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public double[] getB() {
+        return b;
+    }
+
+    public void setB(double[] b) {
+        this.b = b;
+    }
 }
